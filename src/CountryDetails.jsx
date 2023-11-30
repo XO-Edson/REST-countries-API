b133513ba975ee18a887b64150dcd2c2cf0a6f0c
Shaiyo <edsonmike053@gmail.com>
2023-11-30 23:14:52 +0300

    country details

--- src/CountryDetails.jsx
+++ src/CountryDetails.jsx
@@ -12,15 +12,35 @@ export const CountryDetails = ({ data }) => {
   }
 
   return (
-    <div className="Country-details">
-      <img src={countryInfo.flags.png} alt={countryInfo.name.common} />
-      <div>
-        <h2>{countryInfo.name.common}</h2>
-
-        <p>Population: {countryInfo.population}</p>
-        <p>Region: {countryInfo.region}</p>
-        <p>Capital: {countryInfo.capital}</p>
+    <>
+      <div className="search-bar">
+        <button>Back</button>
       </div>
-    </div>
+      <div className="country-details">
+        <img src={countryInfo.flags.png} alt={countryInfo.name.common} />
+
+        <div>
+          <h2>{countryInfo.name.common}</h2>
+
+          <p>Native Name: {}</p>
+          <p>Population: {countryInfo.population}</p>
+          <p>Region: {countryInfo.region}</p>
+          <p>Sub Region:{countryInfo.subregion}</p>
+          <p>Capital:{countryInfo.capital}</p>
+          <p>Top Level Domain:{countryInfo.tld}</p>
+          {/*   <p>Currencies: {countryInfo.currencies.EUR.name}</p> */}
+          <p>Languages:{countryInfo.languages.est}</p>
+
+          <div className="borders">
+            <ul>
+              <p>Border Countries:</p>
+              {countryInfo.borders.map((borderCountry, index) => (
+                <li key={index}>{borderCountry}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
